f7f8740e1a0eee4dbf5a45b24e6fe7dd2788277a
Rodrigo Coelho <rodrigomjcoelho@hotmail.com>
2025-05-19 01:07:00 -0300

    Teste novo ataque da pircareta

--- Assets/Scripts/Items/Pickaxe.cs
+++ Assets/Scripts/Items/Pickaxe.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Pickaxe : Weapon 
 {
+    private Animator animator;
+    private void Start()
+    {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+    }
 
     public override void Attack()
     {
         GameObject pickaxeModel = transform.GetChild(0).gameObject;
-        Collider pickaxeCollider  = GetComponent<Collider>();  
+        Collider pickaxeCollider  = transform.GetChild(0).GetComponent<Collider>();  
 
-        if (UserInputManager.instance.PickaxeInput && !UserInputManager.instance.SprayInput)
+        if (UserInputManager.instance.PickaxeInput && !UserInputManager.instance.SprayInput && animator.GetCurrentAnimatorStateInfo(animator.GetLayerIndex("Base Layer")).length > animator.GetCurrentAnimatorStateInfo(animator.GetLayerIndex("Base Layer")).normalizedTime)
         {
+            if(animator.GetFloat("PickaxeRotation") == 0f)
+            {
+                animator.SetFloat("PickaxeRotation", 1f);
+            }else
+            {
+                animator.SetFloat("PickaxeRotation", 0f);
+            }
             pickaxeModel.SetActive(true);
             pickaxeCollider.enabled = true;
+
         }
         else
         {
